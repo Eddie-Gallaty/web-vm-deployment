a83b832ce3b4b45e0130f7afd2a3794627336557
Eddie Gallaty <eddiegallaty@live.com>
2022-09-25 00:06:05 -0400

    added cluster, datastore, network lists

--- deploy.py
+++ deploy.py
@@ -1,15 +1,19 @@
 from cgi import test
+from inspect import ClosureVars
 from flask import Flask, render_template
 from vm import VM
 
 app = Flask(__name__)
+#new vm object
 vm = VM()
-getvms = []
-getvms = vm.get_vmName()
-print(getvms)
+
+#  get folders and clusters list
+folders = vm.get_folders()
+clusters = vm.get_clusters()
+datastores = vm.get_datastores()
+networks = vm.get_networks()
+
 @app.route('/index')
 def index():
-    vm = VM()
-    getvms = vm.get_vmName()
-    print(getvms)
-    return render_template("index.html", mytitle='Hello World!', getvms=getvms, vms="VMs")
+    
+    return render_template("index.html", mytitle='Hello World!', folders=folders, clusters=clusters, datastores=datastores, networks=networks)
--- vm.py
+++ vm.py
@@ -1,3 +1,4 @@
+from operator import ne
 import requests
 import urllib3
 from pprint import pprint
@@ -14,13 +15,13 @@ session.verify = False
 vsphere_client = create_vsphere_client(server='', username='', password='', session=session)
 
 #  **RESEARCH THIS!*** var for  disable warning for insecure request warnings ** This is not recommended in a production environment.**
-urllibvar = urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+#urllibvar = urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class VM():
     def __init__(self):
         pass
     
-    def get_vmName(self):
+    def get_folders(self):
         vsphere_client
         # Disable cert verification for demo purpose. 
         # This is not recommended in a production environment.
@@ -29,12 +30,40 @@ class VM():
         # This is not recommended in a production environment.
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         # List all VMs inside the vCenter Server
-        list_of_vms = vsphere_client.vcenter.VM.list()
+        list_of_folders = vsphere_client.vcenter.Folder.list()
         #pprint(list_of_vms) #test
-        vmname = []
-        for vm in list_of_vms:
-            vmname.append(vm.name)
-        return vmname
+        folders = []
+        for folder in list_of_folders:
+            folders.append(folder.name)
+        return folders
+    
+    def get_clusters(self):
+        vsphere_client
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        list_of_clusters = vsphere_client.vcenter.Cluster.list()
+        clusters = []
+        for cluster in list_of_clusters:
+            clusters.append(cluster.name)
+        return clusters
+    
+    def get_datastores(self):
+        vsphere_client
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        list_of_datastores = vsphere_client.vcenter.Datastore.list()
+        datastores = []
+        for datastore in list_of_datastores:
+            datastores.append(datastore.name)
+        return datastores
+
+    def get_networks(self):
+        vsphere_client
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        list_of_networks = vsphere_client.vcenter.Network.list()
+        networks = []
+        for network in list_of_networks:
+            networks.append(network.name)
+        return networks
+
 
 
 
