4ee2b8b005322e5d0a7c5adb042ac7eb04ba9111
Eddie Gallaty <eddiegallaty@live.com>
2022-10-01 23:18:32 -0400

    converted to pyvmomi only and added create folder

--- deploy.py
+++ deploy.py
@@ -1,7 +1,10 @@
 from cgi import test
+import dis
 from inspect import ClosureVars
 from flask import Flask, render_template, request
-from vminfo import Vminfo
+from vminfoT import Vminfo
+from pyVim.connect import SmartConnect, Disconnect
+
 
 app = Flask(__name__)
 #new vminfo object
@@ -13,13 +16,21 @@ def index():
     vcenter_name = request.form.get("vcentername")
     username = request.form.get("username")
     password = request.form.get("password")
+    network_select = request.form.get("Networks")
+    create_folder = request.form.get("new_folder")
 
-    # on "submit" button click
     if request.method == 'POST':
-        folders = vminfo.get_folders(vcenter_name, username, password)
-        clusters = vminfo.get_clusters(vcenter_name, username, password)
-        datastores = vminfo.get_datastores(vcenter_name,username, password)
-        networks = vminfo.get_networks(vcenter_name, username, password)
-        return render_template("/index.html", mytitle='Hello World!', folders=folders, clusters=clusters, datastores=datastores, networks=networks)
-    else:
-        return render_template("/index.html", mytitle='Hello World!')
+        if 'submit' in request.form :
+            folders = vminfo.get_folders(vcenter_name, username, password)
+            clusters = vminfo.get_clusters(vcenter_name, username, password)
+            datastores = vminfo.get_datastores(vcenter_name,username, password)
+            networks = vminfo.get_networks(vcenter_name, username, password)
+            return render_template("/index.html", mytitle='Hello World!', folders=folders, clusters=clusters, datastores=datastores, networks=networks)
+        elif 'add' in request.form:
+            #this is testing passing input from textbox to variables
+            print(network_select)
+        elif 'create_folder' in request.form:
+            #create new folder
+            vminfo.create_vm_folder(vcenter_name, username, password, create_folder)
+
+    return render_template("/index.html", mytitle='Hello World!')
--- vminfo.py
+++ vminfo.py
@@ -2,58 +2,98 @@ import requests
 import urllib3
 from pprint import pprint
 from vmware.vapi.vsphere.client import create_vsphere_client
-from pyVim.connect import SmartConnect, Disconnect
 from com.vmware.vcenter_client import (Folder)
-
+from pyVmomi import vim, vmodl
+from pyVim.connect import SmartConnect, Disconnect
 # request to create a session
 session = requests.session()
 # ****research what this is actually doing***** Disable cert verification for demo purpose. This is not recommended in a production environment!!!
 session.verify = False
 
 class Vminfo():
+    
     def __init__(self):
        # ***RESEARCH THIS MORE*** Disable the secure connection warning for demo purpose. This is not recommended in a production environment!!!!.
-       urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    
+       # urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+       pass
+    def get_obj(content, vimtype, name):
+        """
+        Get the vsphere object associated with a given text name
+        """
+        obj = None
+        container = content.viewManager.CreateContainerView(content.rootFolder, vimtype, True)
+        print(container)
+        for c in container.view:
+            if c.name == name:
+                obj = c
+                print("from getobj ", c.name)
+                break
+        return obj
+            
+    def get_objs(self, si, vimtype, recursive):
+        """
+        Get the vsphere object associated with a given text name
+        """
+        obj = []
+        container = si.content.rootFolder #starting point to look into
+        viewType = vimtype #object types to look for
+        container_view = si.content.viewManager.CreateContainerView(container, viewType, recursive) #create view
+        print(container)
+        for child in container_view.view:
+            obj.append(child.name)
+        return obj
+
     def get_folders(self, server, un, pw):
-        vsphere_client = create_vsphere_client(server=server, username=un, password=pw, session=session)
-        # List all VM folders inside the datacenter
-        list_of_folders = vsphere_client.vcenter.Folder.list(Folder.FilterSpec(type=Folder.Type.VIRTUAL_MACHINE))
-        #pprint(list_of_vms) #test
+        si = SmartConnect(host=server, user=un, pwd=pw, port=443, disableSslCertValidation= True)
+        list_of_folders = self.get_objs(si, [vim.Folder], True)
         folders = []
         for folder in list_of_folders:
-            folders.append(folder.name)
+            folders.append(folder)
         return folders
-
+        #### Below is an example for doing this same operation in the vsphere automation sdk ####
+        #vsphere_client = create_vsphere_client(server=server, username=un, password=pw, session=session)
+        # List all VM folders inside the datacenter
+       #list_of_folders = vsphere_client.vcenter.Folder.list(Folder.FilterSpec(type=Folder.Type.VIRTUAL_MACHINE))
+        #ignore this
+        #pprint(list_of_vms) 
+    
     def get_clusters(self, server, un, pw):
-        vsphere_client = create_vsphere_client(server=server, username=un, password=pw, session=session)
-        #list all clusters in the vCenter Server
-        list_of_clusters = vsphere_client.vcenter.Cluster.list()
+        si = SmartConnect(host=server, user=un, pwd=pw, port=443, disableSslCertValidation=True)
+        list_of_clusters = self.get_objs(si, [vim.ClusterComputeResource], True)
         clusters = []
         for cluster in list_of_clusters:
-            clusters.append(cluster.name)
+            clusters.append(cluster)
         return clusters
+
     
     def get_datastores(self, server, un, pw):
-        vsphere_client = create_vsphere_client(server=server, username=un, password=pw, session=session)
-        #list all datastores in the vCenter Server
-        list_of_datastores = vsphere_client.vcenter.Datastore.list()
+        si = SmartConnect(host=server, user=un, pwd=pw, port=443, disableSslCertValidation=True)
+        list_of_datastores = self.get_objs(si, [vim.Datastore], True)
         datastores = []
         for datastore in list_of_datastores:
-            datastores.append(datastore.name)
+            datastores.append(datastore)
         return datastores
+    
+    def get_datastore_clusters(self, server, un, pw):
+        si = SmartConnect(host=server, user=un, pwd=pw, port=443, disableSslCertValidation=True)
+        list_of_datastoreClusters = self.get_objs(si, [vim.StoragePod], True)
+        datastore_clusters = []
+        for dsc in list_of_datastoreClusters:
+            datastore_clusters.append(dsc)
+        return datastore_clusters
+
+
 
     def get_networks(self, server, un, pw):
-        vsphere_client = create_vsphere_client(server=server, username=un, password=pw, session=session)
-        #list all networks in the vCenter Server
-        list_of_networks = vsphere_client.vcenter.Network.list()
+        si = SmartConnect(host=server, user=un, pwd=pw, port=443, disableSslCertValidation=True)
+        list_of_networks = self.get_objs(si, [vim.Network], True)
         networks = []
         for network in list_of_networks:
-            networks.append(network.name)
+            networks.append(network)
         return networks
-
-    def create_vm_folder(self, host, un, pw):
+    
+    def create_vm_folder(self, host, un, pw, folder_name):
         si = SmartConnect(host=host, user=un, pwd=pw, port=443, disableSslCertValidation = True)
         dc = si.content.rootFolder.childEntity[0]
-        dc.vmFolder.CreateFolder("TestEGDev2")
+        dc.vmFolder.CreateFolder(folder_name)
         Disconnect(si)
